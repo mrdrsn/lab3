1702e7036b8f572d222ddd04c03427b827c002c6
mrdrsn <nsokolova127@gmail.com>
2025-04-17 10:17:43 +0300

    xml yml handlers

--- src/main/java/com/mycompany/lab3/Lab3.java
+++ src/main/java/com/mycompany/lab3/Lab3.java
@@ -13,11 +13,13 @@ public class Lab3 {
 
     public static void main(String[] args) {
 // Укажите путь к вашему XML-файлу
-        String fileName = "C:\\Users\\nsoko\\OneDrive\\Desktop\\бибукс\\6 сем\\лаба 3 смирнов\\monsters.xml";
+//        String fileName = "C:\\Users\\nsoko\\OneDrive\\Desktop\\бибукс\\6 сем\\лаба 3 смирнов\\monsters.xml";
+        String fileName = "C:\\Users\\Nastya\\Desktop\\monsters.xml";
+//        String fileName = "C:\\Users\\Nastya\\Desktop\\monsters.yml";
 
         // Вызов парсера
-        List<Monster> monsters = XMLHandler.parseXMLfile(fileName);
-
+        List<Monster> monsters = YAMLHandler.parseYAMLFile(fileName);
+        
         // Вывод результатов на экран
         for (Monster monster : monsters) {
             System.out.println(monster);
--- src/main/java/com/mycompany/lab3/YAMLHandler.java
+++ src/main/java/com/mycompany/lab3/YAMLHandler.java
@@ -4,10 +4,54 @@
  */
 package com.mycompany.lab3;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.yaml.snakeyaml.Yaml;
+//import org.apache.commons.exec.ExecuteException;
+import org.yaml.snakeyaml.error.YAMLException;
+
 /**
  *
  * @author nsoko
  */
 public class YAMLHandler {
-    
+
+    public static List<Monster> parseYAMLFile(String fileName) {
+        List<Monster> monsterList = new ArrayList<>();
+        try {
+            Yaml yaml = new Yaml();
+
+            FileInputStream inputStream = new FileInputStream(fileName);
+            Map<String, Object> yamlData = yaml.load(inputStream);
+
+            List<Map<String, Object>> monstersData = (List<Map<String, Object>>) yamlData.get("monsters");
+
+            for (Map<String, Object> monsterData : monstersData) {
+                Monster monster = new Monster();
+                monster.setCategory((String) monsterData.get("category"));
+                monster.setDescription((String) monsterData.get("description"));
+                monster.setDanger(((Number) monsterData.get("danger")).intValue());
+                monster.setLocation((String) monsterData.get("location"));
+                monster.setFirstMention((String) monsterData.get("first_mentioned"));
+                monster.setHeight(monsterData.get("height") instanceof Double ? (Double) monsterData.get("height") : 0.0);
+                monster.setWeight(monsterData.get("weight") instanceof Integer ? (Integer) monsterData.get("weight") : 0);
+                monster.setVulnerability(monsterData.get("vulnerability") instanceof String ? (String) monsterData.get("vulnerability") : " ");
+                monster.setImmune((ArrayList<String>) monsterData.get("immune"));
+                monster.setActive((String) monsterData.get("active"));
+                monster.setRecipe((String) monsterData.get("recipe"));
+                monster.setTime((int) monsterData.get("time"));
+                monster.setEfficiency((String) monsterData.get("efficiency"));
+                monsterList.add(monster);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (YAMLException e){
+            System.out.println("это не yml файл");
+        }
+        return monsterList;
+    }
 }
